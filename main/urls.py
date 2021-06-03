26f12b39f56ebc558feebe7970be1f979b6b590a
counter0021 <ara123rus123ara@gmail.com>
2021-06-03 13:18:46 +0300

    Stream video

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BaseView, VideoDetailView
+from .views import BaseView, VideoDetailView, VideoStreamingResponse
 
 app_name = 'main'
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
     path('video/<str:slug>/', VideoDetailView.as_view(), name='video_detail'),
+    path('video/streaming/<str:slug>/', VideoStreamingResponse.as_view(), name='video_streaming')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+from django.http import FileResponse
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 
 from .models import Video
 
@@ -19,3 +20,12 @@ class VideoDetailView(DetailView):
     context_object_name = 'video'
     template_name = 'video_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class VideoStreamingResponse(View):
+    """ Video player (chunked video upload) """
+
+    def get(self, request, *args, **kwargs):
+        video = Video.objects.get(slug=kwargs['slug'])
+        response = FileResponse(open(video.file.path, 'rb'))
+        return response
