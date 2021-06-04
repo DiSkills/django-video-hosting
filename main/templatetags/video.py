a3912c04c223a5d4b6ba113ce1764ba2646f5185
counter0021 <ara123rus123ara@gmail.com>
2021-06-04 19:13:49 +0300

    Likes and Dislikes fix

--- main/templatetags/video.py
+++ main/templatetags/video.py
@@ -7,9 +7,16 @@ register = template.Library()
 
 @register.simple_tag()
 def likes_or_dislikes_count(video, vote):
+    """ Count likes or dislikes from video """
+
     return LikeOrDislike.objects.filter(video=video, vote=vote).count()
 
 
 @register.simple_tag()
 def get_vote_user(video, user):
-    return LikeOrDislike.objects.get(video=video, user=user).vote
+    """ Get vote user """
+
+    vote = LikeOrDislike.objects.filter(video=video, user=user)
+    if vote:
+        return vote.first().vote
+    return ''
